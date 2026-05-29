b009b0def9ad65b92925c6444f3e85813e20a759
srihithac <90529172+srihithac@users.noreply.github.com>
2026-05-29 11:51:35 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,14 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = Counter(nums)
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [num for count, num in heap]
+        
